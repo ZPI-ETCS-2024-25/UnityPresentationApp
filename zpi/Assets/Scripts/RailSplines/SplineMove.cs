dee8ab024c1f59c5199c0ac4b38724733656e74f
jakubszwiec <96255055+jakubszwiec@users.noreply.github.com>
2024-10-17 20:02:41 +0200

    Added train model and wagon following it
    -train asset with materials from: https://assetstore.unity.com/packages/3d/environments/urban/toon-city-pack-234785
    -swapped cube model for train model
    -added ability for multiple wagons to follow the train

--- zpi/Assets/Scripts/RailSplines/SplineMove.cs
+++ zpi/Assets/Scripts/RailSplines/SplineMove.cs
@@ -6,6 +6,7 @@ public class SplineMove : MonoBehaviour
 {
     public UnityETCSComm comm;
     public SplineContainer spline;
+    [SerializeField] private GameObject[] wagons;
     private float speed = 0f;
     private float distancePercentage = 0f;
     private int lastBalise = 0;
@@ -13,21 +14,57 @@ public class SplineMove : MonoBehaviour
     public float acceleration = 0.2f;
 
     float splineLength;
+    float trainScaleMultiplier;
 
     void Start()
     {
         splineLength = spline.CalculateLength();
         speed = startingSpeed;
+        trainScaleMultiplier = transform.localScale.x;
+
+        float wagonsLength = 0.0f;
+        for (int i = 0; i < wagons.Length; i++)
+        {
+            //this model has its length as y
+            wagonsLength += wagons[i].GetComponent<BoxCollider>().size.y * trainScaleMultiplier;
+        }
+        distancePercentage += wagonsLength / splineLength;
+    }
+
+    private void MoveWagons(float distancePercentage)
+    {
+        for (int i = 0; i < wagons.Length; i++)
+        {
+            float wagonDistancePercentage = distancePercentage - ((wagons[i].GetComponent<BoxCollider>().size.y ) * (i+1) * trainScaleMultiplier - GetComponent<BoxCollider>().size.y * 0.47f) / splineLength;
+            Vector3 wagonPosition = spline.EvaluatePosition(wagonDistancePercentage);
+            wagons[i].transform.position = wagonPosition;
+
+            Vector3 nextPosition = spline.EvaluatePosition(wagonDistancePercentage + 0.05f);
+            Vector3 direction = nextPosition - wagonPosition ;
+            wagons[i].transform.rotation = Quaternion.LookRotation(direction, transform.up);
+            //fix rotation of wagon
+            wagons[i].transform.Rotate(-90, 0, 0);
+        }
     }
 
     void FixedUpdate()
     {
         distancePercentage += speed * Time.deltaTime / splineLength;
         speed = speed + acceleration;
+        
+        if (distancePercentage > 1f)
+        {
+            distancePercentage = 1f;
+        }
+        if (distancePercentage < 0f)
+        {
+            distancePercentage = 0f;
+        }
 
         Vector3 currentPosition = spline.EvaluatePosition(distancePercentage);
         transform.position = currentPosition;
 
+        MoveWagons(distancePercentage);
 
         BaliseController baliseController = spline.GetComponent<BaliseController>();
         if(baliseController != null)
@@ -46,14 +83,7 @@ public class SplineMove : MonoBehaviour
         }
         
 
-        if (distancePercentage > 1f)
-        {
-            distancePercentage = 1f;
-        }
-        if(distancePercentage < 0f)
-        {
-            distancePercentage = 0f;
-        }
+        
 
         Vector3 nextPosition = spline.EvaluatePosition(distancePercentage + 0.05f);
         Vector3 direction = nextPosition - currentPosition;
