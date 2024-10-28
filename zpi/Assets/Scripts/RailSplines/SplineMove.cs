fcf54ba59c59ec59f840c2caf74e1e37ea587048
WeNeedToCodeDeeper <rafal.trzak@gmail.com>
2024-10-28 18:20:54 +0100

    Spline move update
    -now wagons move with train

--- zpi/Assets/Scripts/RailSplines/SplineMove.cs
+++ zpi/Assets/Scripts/RailSplines/SplineMove.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Splines;
 using System;
+using System.Numerics;
 
 public class SplineMove : MonoBehaviour
 {
@@ -11,10 +12,13 @@ public class SplineMove : MonoBehaviour
     public SplineContainer splineContainer;
     public int startingSpline;
     private int currentSpline;
+    private List<int> standingOnSplines;
 
 
     [SerializeField]
     private GameObject[] wagons;
+    private int[] wagonSplines;
+    private float[] wagonDistance;
 
 
     public float startingSpeed = 1f;
@@ -22,73 +26,54 @@ public class SplineMove : MonoBehaviour
     public float rotationCalculation = 0.05f;
     private float speed = 0f;
 
+
     private float distancePercentage = 0f;
     private float previousDistancePercentage = 0f;
 
 
     public float wagonSeparationDistance = 0.03f;
-    float splineLength;
-    float trainScaleMultiplier;
 
 
     void Start()
     {
         currentSpline = startingSpline;
+        standingOnSplines = new List<int>() { currentSpline };
         speed = startingSpeed;
 
-
-        trainScaleMultiplier = transform.localScale.x;
-        splineLength = splineContainer.CalculateLength(currentSpline);
-
-        float wagonsLength = 0.0f;
-        for (int i = 0; i < wagons.Length; i++)
-        {
-            //this model has its length as y
-            wagonsLength += wagons[i].GetComponent<BoxCollider>().size.y * trainScaleMultiplier;
-        }
-
-        distancePercentage += wagonsLength / splineLength;
+        InitialiseWagons();
     }
 
-    private void MoveWagons(float distancePercentage)
+    /*private void MoveWagons(float distancePercentage)
     {
         for (int i = 0; i < wagons.Length; i++)
         {
-            float wagonDistancePercentage = distancePercentage - ((wagons[i].GetComponent<BoxCollider>().size.y ) * (i+1) * trainScaleMultiplier - GetComponent<BoxCollider>().size.y * (0.5f - wagonSeparationDistance)) / splineLength;
+            float wagonDistancePercentage = distancePercentage - ((wagons[i].GetComponent<BoxCollider>().size.y ) * (i+1) * transform.localScale.x - GetComponent<BoxCollider>().size.y * (0.5f - wagonSeparationDistance)) / splineContainer.CalculateLength(currentSpline);
             Vector3 wagonPosition = splineContainer.EvaluatePosition(currentSpline,wagonDistancePercentage);
             wagons[i].transform.position = wagonPosition;
 
             RotateObject(wagons[i], currentSpline, wagonDistancePercentage);
             wagons[i].transform.Rotate(-90, 0, 0); //correct rotation
         }
-    }
+    }*/
 
     void FixedUpdate()
     {
         previousDistancePercentage = distancePercentage;
         speed = speed + acceleration;
 
-        //distancePercentage += speed * Time.deltaTime / splineLength;
-
-        /*
-        if (distancePercentage > 1f)
-        {
-            distancePercentage = 1f;
-        }
-        if (distancePercentage < 0f)
-        {
-            distancePercentage = 0f;
-        }*/
-
-        //Vector3 currentPosition = splineContainer.EvaluatePosition(currentSpline, distancePercentage);
-        //transform.position = currentPosition;
-        MoveObject(gameObject, currentSpline, speed, distancePercentage,out distancePercentage,out currentSpline);
+        
+        MoveTrain(gameObject, currentSpline, speed, distancePercentage,out distancePercentage,out currentSpline);
         RotateObject(gameObject, currentSpline, distancePercentage);
         //MoveWagons(distancePercentage);
 
+        if (!standingOnSplines.Contains(currentSpline))
+        {
+            standingOnSplines.Add(currentSpline);  
+        }
 
-
-
+        MoveWagons(speed);
+        
+        
         BaliseController baliseController = splineContainer.GetComponent<BaliseController>();
         if(baliseController != null)
         {
@@ -113,16 +98,16 @@ public class SplineMove : MonoBehaviour
         if(previousDistancePercentage != distancePercentage)
         {
             float rotationDistanceCalculation = Mathf.Min(1f, distancePercentage + rotationCalculation);
-            Vector3 nextPosition = splineContainer.EvaluatePosition(spline, rotationDistanceCalculation);
-            Vector3 direction = nextPosition - rotatingObject.transform.position;
-            if(direction != Vector3.zero)
+            UnityEngine.Vector3 nextPosition = splineContainer.EvaluatePosition(spline, rotationDistanceCalculation);
+            UnityEngine.Vector3 direction = nextPosition - rotatingObject.transform.position;
+            if(direction != UnityEngine.Vector3.zero)
             {
-                rotatingObject.transform.rotation = Quaternion.LookRotation(direction, transform.up);
+                rotatingObject.transform.rotation = UnityEngine.Quaternion.LookRotation(direction, transform.up);
             }
         }
     }
 
-    private void MoveObject(GameObject gameObject,int spline,float speed, float currenctDistance, out float afterMovingDistance, out int afterMovingSpline)
+    private void MoveTrain(GameObject gameObject,int spline,float speed, float currenctDistance, out float afterMovingDistance, out int afterMovingSpline)
     {
         float splineLength = splineContainer.CalculateLength(spline);
         float percentageTravelled = speed * Time.fixedDeltaTime / splineLength;
@@ -157,5 +142,90 @@ public class SplineMove : MonoBehaviour
             }
             
         }
+    }
+
+
+    private void MoveWagons(float speed)
+    {
+        for(int i = 0; i < wagons.Length; i++)
+        {
+            float splineLength = splineContainer.CalculateLength(wagonSplines[i]);
+            float percentageTravelled = speed * Time.fixedDeltaTime / splineLength;
+
+            if (wagonDistance[i] + percentageTravelled <= 1f)
+            {
+                wagons[i].transform.position = splineContainer.EvaluatePosition(wagonSplines[i], wagonDistance[i] + percentageTravelled);
+                wagonDistance[i] = wagonDistance[i] + percentageTravelled;
+                RotateObject(wagons[i], wagonSplines[i], wagonDistance[i]);
+                wagons[i].transform.Rotate(-90, 0, 0); //correct rotation
+            }
+            else
+            {
+                try
+                {
+                    int currentSpline = standingOnSplines.IndexOf(wagonSplines[i]);
+                    int newSpline = standingOnSplines[currentSpline + 1];
+                    if (i == 0)
+                    {
+                        standingOnSplines.RemoveAt(0);
+                    }
+                    float moveLeftover = wagonDistance[i] + percentageTravelled - 1f;
+                    float newSplineLenght = splineContainer.CalculateLength(newSpline);
+                    float percentageTravelledNewSpline = moveLeftover * splineLength / newSplineLenght;
+
+                    wagonSplines[i] = newSpline;
+                    wagonDistance[i] = percentageTravelledNewSpline;
+
+                    wagons[i].transform.position = splineContainer.EvaluatePosition(wagonSplines[i], wagonDistance[i]);
+                    RotateObject(wagons[i], wagonSplines[i], wagonDistance[i]);
+                    wagons[i].transform.Rotate(-90, 0, 0); //correct rotation
+                }
+                catch
+                {
+                    wagons[i].transform.position = splineContainer.EvaluatePosition(wagonSplines[i], 1f);
+                    wagonDistance[i] = 1f;
+                    RotateObject(wagons[i], wagonSplines[i], wagonDistance[i]);
+                    wagons[i].transform.Rotate(-90, 0, 0); //correct rotation
+                }
+                
+            }
+        }
+    }
+
+
+    private void InitialiseWagons()
+    {
+        wagonSplines = new int[wagons.Length];
+        for (int i = 0; i < wagonSplines.Length; i++)
+        {
+            wagonSplines[i] = startingSpline;
+        }
+
+
+        wagonDistance = new float[wagons.Length];
+
+
+        InitialiseWagonsPlacement();
+    }
+
+
+    private void InitialiseWagonsPlacement()
+    {
+        float trainScaleMultiplier = transform.localScale.x;
+        float splineLength = splineContainer.CalculateLength(currentSpline);
+
+        float wagonsLenghtSum = 0.0f;
+        for (int i = 0; i < wagons.Length; i++)
+        {
+            //this model has its length as y
+            float wagonsLength = wagons[i].GetComponent<BoxCollider>().size.y * trainScaleMultiplier;
+            wagonsLenghtSum += wagonsLength;
+
+            for (int j = wagons.Length-1; j > i; j--)
+            {
+                wagonDistance[j] = wagonDistance[j] + (wagonsLength / splineLength);
+            }
+        }
+        distancePercentage += wagonsLenghtSum / splineLength;
     }
 }
